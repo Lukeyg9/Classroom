049c0686568c89a77f29d18e2c506cf1c7f3415c
Lukeyg9 <156789897+Lukeyg9@users.noreply.github.com>
2024-04-29 14:21:49 +0100

    Update Car.py

--- Car.py
+++ Car.py
@@ -10,3 +10,12 @@ with open("output.csv") as csvfile:
         sales.append([int(x.replace(",", "")) for x in row [1:]])
 
 monthly_sum = [sum(x) for x in zip(*sales)]
+
+yearly_sum = {}
+for i in range(len(companies)):
+    yearly_sum[companies[i]] = sum(sales[i])
+
+print(monthly_sum)
+print("yearly sales: ")
+for company, sales in yearly_sum.items():
+    print(company, sales)
