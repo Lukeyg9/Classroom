b01ecdd37b21ab984ef6934636798fe69318bc7e
Lukeyg9 <156789897+Lukeyg9@users.noreply.github.com>
2024-04-30 12:09:23 +0100

    Update Count_Pylint.py

--- Pylint/Count_Pylint.py
+++ Pylint/Count_Pylint.py
@@ -1,9 +1,6 @@
 # pylint: disable=invalid-name
 
-def count(sequence, item):
-    """
-    Count the occurrences of 'item' in 'sequence'.
-    """
+
     total_count = 0
     for element in sequence:
         if element == item:
@@ -11,17 +8,13 @@ def count(sequence, item):
     return total_count
 
 def test_count_with_integers():
-    """
-    Test count function with a list of integers.
-    """
+
     sequence = [1, 2, 3, 4, 5, 2, 2]
     item = 2
     assert count(sequence, item) == 3, "Counting integers failed"
 
 def test_count_with_strings():
-    """
-    Test count function with a list of strings.
-    """
+
     sequence = ["apple", "banana", "orange", "apple", "grape"]
     item = "apple"
     assert count(sequence, item) == 2, "Counting strings failed"
