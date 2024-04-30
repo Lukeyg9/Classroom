ec427dc24483eef5ef3b84fe274053276db6c7f2
Lukeyg9 <156789897+Lukeyg9@users.noreply.github.com>
2024-04-30 14:25:45 +0100

    Create Task2.py

---
+++ Functions Challenges/Task2.py
@@ -0,0 +1,11 @@
+def perform_operations():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    sum_result = num1 + num2
+    sub_result = num1 - num2
+    multiply_result = num1 * num2
+    
+    print("Sum is {}, sub is {}, and multiply is {}".format(sum_result, sub_result, multiply_result))
+
+perform_operations()
