41d4848846c79db96070830353dc20b02418b2a9
Lukeyg9 <156789897+Lukeyg9@users.noreply.github.com>
2024-04-30 14:29:01 +0100

    Create Task5.py

---
+++ Functions Challenges/Task5.py
@@ -0,0 +1,6 @@
+def convert_to_uppercase(input_string):
+    return input_string.upper()
+
+user_input = input("Enter a string: ")
+uppercase_string = convert_to_uppercase(user_input)
+print("Uppercase string:", uppercase_string)
