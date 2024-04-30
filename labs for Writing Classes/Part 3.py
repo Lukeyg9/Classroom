f72a939a68ae02618c4a216a3b83c3f9b4ec6f18
Lukeyg9 <156789897+Lukeyg9@users.noreply.github.com>
2024-04-30 12:49:15 +0100

    Create Part 3.py

---
+++ labs for Writing Classes/Part 3.py
@@ -0,0 +1,30 @@
+class Bird:
+    def __init__(self, species, habitat):
+        self.species = species
+        self.habitat = habitat
+
+    def make_sound(self):
+        raise NotImplementedError("Subclasses must use make_sound.")
+
+class Owl(Bird):
+    def __init__(self, species, habitat, nocturnal=True):
+        super().__init__(species, habitat)
+        self.nocturnal = nocturnal
+
+    def make_sound(self):
+        return "Hoot!"
+
+class Dodo(Bird):
+    def __init__(self, species, habitat, extinct=True):
+        super().__init__(species, habitat)
+        self.extinct = extinct
+
+    def make_sound(self):
+        return "Silence..."
+
+owl = Owl("Owl", "Forest")
+dodo = Dodo("Dodo", "Island")
+
+birds = [owl, dodo]
+for bird in birds:
+    print(f"{bird.species} lives in {bird.habitat}. Sound: {bird.make_sound()}")
